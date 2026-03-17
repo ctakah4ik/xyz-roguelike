1ae2fc61c2b680be01b44fccd30f333353cadfbc
Igor Simonov <ragged.bag@gmail.com>
2026-03-17 12:00:36 +0100

    [#4] Remove all RoguelikeGame source files for project cleanup

---
+++ XYZRoguelike/DeveloperLevel.cpp
@@ -0,0 +1,20 @@
+#include "DeveloperLevel.h"
+
+using namespace XYZEngine;
+
+namespace XYZRoguelike
+{
+	void DeveloperLevel::Start()
+	{
+		player = std::make_shared<Player>();
+	}
+	void DeveloperLevel::Restart()
+	{
+		Stop();
+		Start();
+	}
+	void DeveloperLevel::Stop() 
+	{
+		GameWorld::Instance()->Clear();
+	}
+}
